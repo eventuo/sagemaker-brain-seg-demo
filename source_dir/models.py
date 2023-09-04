7ce782368cacfa798f92ab3a51af9c061c3a8ce0
eventuo <eventuoto@proton.me>
2023-09-04 18:56:04 +0000

    More clean-up

--- source_dir/models.py
+++ source_dir/models.py
@@ -13,4 +13,22 @@ def _conv_block(inp, num_filter, kernel, pad, block, conv_block):
     conv = mx.sym.Convolution(inp, num_filter=num_filter, kernel=kernel, pad=pad, name='conv%i_%i' % (block, conv_block))
     conv = mx.sym.BatchNorm(conv, fix_gamma=True, name='bn%i_%i' % (block, conv_block))
     conv = mx.sym.Activation(conv, act_type='relu', name='relu%i_%i' % (block, conv_block))
-    re
+    return conv
+
+def _down_block(inp, num_filter, kernel, pad, block, pool=True):
+    conv = _conv_block(inp, num_filter, kernel, pad, block, 1)
+    conv = _conv_block(conv, num_filter, kernel, pad, block, 2)
+    if pool:
+        pool = mx.sym.Pooling(conv, kernel=(2, 2), stride=(2, 2), pool_type='max', name='pool_%i' % block)
+        return pool, conv
+    return conv
+
+def _down_branch(inp):
+    pool1, conv1 = _down_block(inp, num_filter=32, kernel=(3, 3), pad=(1, 1), block=1)
+    pool2, conv2 = _down_block(pool1, num_filter=64, kernel=(3, 3), pad=(1, 1), block=2)
+    pool3, conv3 = _down_block(pool2, num_filter=128, kernel=(3, 3), pad=(1, 1), block=3)
+    pool4, conv4 = _down_block(pool3, num_filter=256, kernel=(3, 3), pad=(1, 1), block=4)
+    conv5 = _down_block(pool4, num_filter=512, kernel=(3, 3), pad=(1, 1), block=5, pool=False)
+    return [conv5, conv4, conv3, conv2, conv1]
+
+def _up_block(
